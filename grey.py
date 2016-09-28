efc5439a82f1fe9ad53e14b518be3cd20ed970ff
liyongchun168 <liyongchun168@yeah.net>
2016-09-28 20:27:09 +0800

    init

---
+++ grey.py
@@ -0,0 +1,20 @@
+from  PIL  import  Image
+from  pylab  import  *
+from numpy import *
+
+im  =  array(Image.open('lena.jpg'))
+#im  =  array(Image.open('tree.jpg'))
+
+#print im.shape,im.dtype
+
+im2 =  255-im
+im3 =  (100.0/255) * im +100
+im4 = 255.0 * (im/255.0)**2
+
+#print int(im2.min()),int(im2.max())
+
+imshow(im4)
+#print  'Please  click  3  points'
+#x  =  ginput(3)
+#print  'you  clicked:',x
+show()
