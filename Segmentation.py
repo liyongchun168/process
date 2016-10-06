383fd140854a8182fc25896416f07fc25658ee65
liyongchun168 <liyongchun168@yeah.net>
2016-10-07 00:41:12 +0800

    1006_2
    2

--- Segmentation.py
+++ Segmentation.py
@@ -3,9 +3,18 @@ from  pylab  import  *
 from numpy import *
 
 
-im  =  array(Image.open('lena.jpg').convert('L'))
+im  =  array(Image.open('lena.jpg'))
 
+
+for i in range(0,im.shape[0]):
+    for j in range(0,im.shape[1]):
+        grey  =   (int(im[i,j,0])+int(im[i,j,1])+int(im[i,j,2]))/3  
+        im[i,j,0]   =   grey
+        im[i,j,1]   =   grey
+        im[i,j,2]   =   grey
+    
     
 imshow(im)
 
 show()
+
